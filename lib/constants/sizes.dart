7ac71ff329b49749d5b8840c30c12a1e8574e330
AmarchandK <amarchand00345@gmail.com>
2022-11-23 13:32:25 +0530

    git ignore

--- lib/constants/sizes.dart
+++ lib/constants/sizes.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 const SizedBox h20 = SizedBox(height: 20);
 const SizedBox h10 = SizedBox(height: 10);
 const SizedBox h5 = SizedBox(height: 5);
-
 const SizedBox h30 = SizedBox(height: 30);
 const SizedBox w10 = SizedBox(width: 10);
 const SizedBox w20 = SizedBox(width: 20);
